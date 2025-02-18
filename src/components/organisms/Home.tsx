19d287b4bf28cb071c2f98e0f145151867f55a1f
Arnav Surve <arnav@surve.dev>
2025-02-18 03:54:13 -0800

    test edit for ci

--- src/components/organisms/Home.tsx
+++ src/components/organisms/Home.tsx
@@ -4,7 +4,7 @@ const Home = () => {
   //https://64.media.tumblr.com/dbfb353e4457dc889bb160ae4a849772/7c4eef3239d7163b-be/s2048x3072/88fb52061037283353688a2f130cee47b6698408.jpg
   return (
     <div>
-      <h1>Arnav Surve</h1>
+      <h1>Arnav Surve test edit</h1>
 
       <a href="https://github.com/arnavsurve">github</a>
       <br />
