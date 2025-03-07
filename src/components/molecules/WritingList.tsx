5d59c469b436eb65dc2dc5daaf73441c6c1a37e5
Arnav Surve <arnav@surve.dev>
2025-03-06 21:33:54 -0800

    scrappingthis

--- src/components/molecules/WritingList.tsx
+++ src/components/molecules/WritingList.tsx
@@ -36,13 +36,15 @@ const WritingList = () => {
   return (
     <div>
       <br />
-      <br />
       <img
         src="https://64.media.tumblr.com/fc431188a913f908c6df298a67969ca7/c1858754c2bc5714-36/s2048x3072/42a134d14bee44f4d348bddc7c0646fcb741af9a.jpg"
         alt=""
         style={{ maxWidth: "250px" }}
       />
-      <h2>Writing</h2>
+      <a href="/writing">
+        <h2 style={{ marginBottom: "0.5rem" }}>Writing</h2>
+      </a>
+      <small>thoughts, notes</small>
       <ul>
         {posts.map((post) => (
           <li key={post.slug}>
