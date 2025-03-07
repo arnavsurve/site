940b1ce46d6004daea29a732154b0194ee749c8d
Arnav Surve <arnav@surve.dev>
2025-03-06 21:34:28 -0800

    links to writing and reading indices

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/organisms/Home";
 import BlogPage from "./components/organisms/BlogPage";
+import BlogIndex from "./components/organisms/BlogIndex";
+import Reading from "./components/organisms/Reading";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/writing" element={<BlogIndex />}></Route>
       <Route path="/writing/:slug" element={<BlogPage />}></Route>
+      <Route path="/reading" element={<Reading />}></Route>
     </Routes>
   );
 };
