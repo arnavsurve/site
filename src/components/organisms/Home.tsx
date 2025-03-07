ae0c1268db8aaa9ed3bbcd67b4da0c485d972496
Arnav Surve <arnav@surve.dev>
2025-03-06 21:34:21 -0800

    redesign, links to writing and reading

--- src/components/organisms/Home.tsx
+++ src/components/organisms/Home.tsx
@@ -1,10 +1,12 @@
-import WritingList from "../molecules/WritingList";
-
 const Home = () => {
   return (
     <div>
       <h2 style={{ marginTop: 0 }}>Arnav Surve</h2>
 
+      <a style={{ marginBottom: "1rem" }} href="https://x.com/1arnavsurve">
+        twitter
+      </a>
+      <br />
       <a href="https://github.com/arnavsurve">github</a>
       <br />
       <a href="https://linkedin.com/in/arnavsurve">linkedin</a>
@@ -12,39 +14,49 @@ const Home = () => {
       <a href="mailto:arnav@surve.dev">arnav@surve.dev</a>
       <br />
       <p>
-        Studying Computer Science at CSU Sacramento. Currently building{" "}
+        I write software and study the human condition. Currently building{" "}
         <a href="https://ynot.lol">Y</a>, a social platform for the{" "}
         <a href="https://en.wikipedia.org/wiki/Semantic_Web">semantic web</a>.
       </p>
       <hr style={{ borderColor: "gray" }} />
 
-      <WritingList />
+      <div>
+        <br />
+        <img
+          src="https://64.media.tumblr.com/fc431188a913f908c6df298a67969ca7/c1858754c2bc5714-36/s2048x3072/42a134d14bee44f4d348bddc7c0646fcb741af9a.jpg"
+          alt=""
+          style={{ maxWidth: "250px" }}
+        />
+        <a href="/writing">
+          <h2 style={{ marginBottom: "0.5rem" }}>Writing</h2>
+        </a>
+        <small>thoughts, notes</small>
+      </div>
 
-      <br />
-      <br />
-      <img
-        src="https://64.media.tumblr.com/4b6908703d6c69c9f949927a22441172/c05ac8de92e52b38-42/s2048x3072/1db0b2c4fd4f89c571865ac3ace9fe8da2d1b9f0.jpg"
-        alt=""
-        style={{ maxWidth: "250px" }}
-      />
-      <h2>Work</h2>
-      <ul>
-        <li>
-          <a href="https://github.com/EaRu723/ynot.lol-web">Y</a>
-        </li>
-        <li>
-          <a href="https://github.com/arnavsurve/promise">promise</a>
-        </li>
-      </ul>
+      <div>
+        <br />
+        <img
+          src="https://64.media.tumblr.com/4b6908703d6c69c9f949927a22441172/c05ac8de92e52b38-42/s2048x3072/1db0b2c4fd4f89c571865ac3ace9fe8da2d1b9f0.jpg"
+          alt=""
+          style={{ maxWidth: "250px" }}
+        />
+        <a href="/reading">
+          <h2 style={{ marginBottom: "0.5rem" }}>Reading</h2>
+        </a>
+        <small>peak literature</small>
+        <br />
+      </div>
 
-      <br />
-      <br />
-      <img
-        src="https://64.media.tumblr.com/dbfb353e4457dc889bb160ae4a849772/7c4eef3239d7163b-be/s2048x3072/88fb52061037283353688a2f130cee47b6698408.jpg"
-        alt=""
-        style={{ maxWidth: "250px" }}
-      />
-      <h2>Notes</h2>
+      <div>
+        <br />
+        <img
+          src="https://64.media.tumblr.com/dbfb353e4457dc889bb160ae4a849772/7c4eef3239d7163b-be/s2048x3072/88fb52061037283353688a2f130cee47b6698408.jpg"
+          alt=""
+          style={{ maxWidth: "250px" }}
+        />
+        <h2 style={{ marginBottom: "0.5rem" }}>Notes</h2>
+        <small>will open source all my notes eventually</small>
+      </div>
     </div>
   );
 };
