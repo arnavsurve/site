04f1304bad281a1dd2d1738b4dd0fd011a3ca926
Arnav Surve <arnav@surve.dev>
2025-10-02 13:33:30 -0700

    simplify everything

--- src/components/organisms/Home.tsx
+++ src/components/organisms/Home.tsx
@@ -15,47 +15,47 @@ const Home = () => {
       <br />
       <p>
         I write software and study the human condition. Currently building{" "}
-        <a href="https://graceinfra.org">Grace</a>.
+        <a href="https://glyfs.dev">Glyfs</a>.
       </p>
-      <hr style={{ borderColor: "gray" }} />
-
-      <div>
-        <br />
-        <img
-          src="https://64.media.tumblr.com/fc431188a913f908c6df298a67969ca7/c1858754c2bc5714-36/s2048x3072/42a134d14bee44f4d348bddc7c0646fcb741af9a.jpg"
-          alt=""
-          style={{ maxWidth: "250px" }}
-        />
-        <a href="/writing">
-          <h2 style={{ marginBottom: "0.5rem" }}>Writing</h2>
-        </a>
-        <small>thoughts, notes</small>
-      </div>
-
-      <div>
-        <br />
-        <img
-          src="https://64.media.tumblr.com/4b6908703d6c69c9f949927a22441172/c05ac8de92e52b38-42/s2048x3072/1db0b2c4fd4f89c571865ac3ace9fe8da2d1b9f0.jpg"
-          alt=""
-          style={{ maxWidth: "250px" }}
-        />
-        <a href="/reading">
-          <h2 style={{ marginBottom: "0.5rem" }}>Reading</h2>
-        </a>
-        <small>peak literature</small>
-        <br />
-      </div>
-
-      <div>
-        <br />
-        <img
-          src="https://64.media.tumblr.com/dbfb353e4457dc889bb160ae4a849772/7c4eef3239d7163b-be/s2048x3072/88fb52061037283353688a2f130cee47b6698408.jpg"
-          alt=""
-          style={{ maxWidth: "250px" }}
-        />
-        <h2 style={{ marginBottom: "0.5rem" }}>Notes</h2>
-        <small>will open source all my notes after I'm famous</small>
-      </div>
+      {/* <hr style={{ borderColor: "gray" }} /> */}
+      {/**/}
+      {/* <div> */}
+      {/*   <br /> */}
+      {/*   <img */}
+      {/*     src="https://64.media.tumblr.com/fc431188a913f908c6df298a67969ca7/c1858754c2bc5714-36/s2048x3072/42a134d14bee44f4d348bddc7c0646fcb741af9a.jpg" */}
+      {/*     alt="" */}
+      {/*     style={{ maxWidth: "250px" }} */}
+      {/*   /> */}
+      {/*   <a href="/writing"> */}
+      {/*     <h2 style={{ marginBottom: "0.5rem" }}>Writing</h2> */}
+      {/*   </a> */}
+      {/*   <small>thoughts, notes</small> */}
+      {/* </div> */}
+      {/**/}
+      {/* <div> */}
+      {/*   <br /> */}
+      {/*   <img */}
+      {/*     src="https://64.media.tumblr.com/4b6908703d6c69c9f949927a22441172/c05ac8de92e52b38-42/s2048x3072/1db0b2c4fd4f89c571865ac3ace9fe8da2d1b9f0.jpg" */}
+      {/*     alt="" */}
+      {/*     style={{ maxWidth: "250px" }} */}
+      {/*   /> */}
+      {/*   <a href="/reading"> */}
+      {/*     <h2 style={{ marginBottom: "0.5rem" }}>Reading</h2> */}
+      {/*   </a> */}
+      {/*   <small>peak literature</small> */}
+      {/*   <br /> */}
+      {/* </div> */}
+      {/**/}
+      {/* <div> */}
+      {/*   <br /> */}
+      {/*   <img */}
+      {/*     src="https://64.media.tumblr.com/dbfb353e4457dc889bb160ae4a849772/7c4eef3239d7163b-be/s2048x3072/88fb52061037283353688a2f130cee47b6698408.jpg" */}
+      {/*     alt="" */}
+      {/*     style={{ maxWidth: "250px" }} */}
+      {/*   /> */}
+      {/*   <h2 style={{ marginBottom: "0.5rem" }}>Notes</h2> */}
+      {/*   <small>will open source all my notes after I'm famous</small> */}
+      {/* </div> */}
     </div>
   );
 };
